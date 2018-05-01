202f952055369f88e0d578e08912674477d8b65b
denissduan <dleijl@163.com>
2018-05-01 12:39:50 +0800

    jenkins init

--- src/test/java/com/se7en/RegularTest.java
+++ src/test/java/com/se7en/RegularTest.java
@@ -8,11 +8,6 @@ public class RegularTest {
 	@Test
 	public void test(){
 		//matches("[\u4E00-\u9FA5]+")
-		Scanner scan = new Scanner(System.in);
-		while(true){
-			String str = scan.nextLine();
-			System.out.println(str.matches("[\u4E00-\u9FA5]+"));
-		}
 	}
 	
 }
